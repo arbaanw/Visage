eb596fbbac263833970d86e9f543950a7ddeb5de
Arbaan <mohammed.arbaan2@gmail.com>
2025-10-02 22:03:33 +0530

    add results folder

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ img = Image.open(img_path)
 img_matrix = np.array(img)
 
 brightness_value = 100
-contrast_value = 2.2
+contrast_value = 1.8
 
 bright_img_matrix = bright.adjust_brightness(img_matrix, brightness_value)
 contrast_img_matrx = contrast.adjust_contrast(img_matrix, contrast_value)
@@ -21,6 +21,6 @@ bright_img = Image.fromarray(bright_img_matrix)
 contrast_img = Image.fromarray(contrast_img_matrx)
 
 
-bright_img.save("bright_result.png")
-contrast_img.save("contrast_result.png")
+bright_img.save("results/bright_result.png")
+contrast_img.save("results/contrast_result.png")
 
