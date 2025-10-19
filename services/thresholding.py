a9606aa2ae44e81cc6b2f721a3e12bfe6acf0b5f
Arbaan <mohammed.arbaan2@gmail.com>
2025-10-19 18:52:11 +0530

    fix thresholding

--- services/thresholding.py
+++ services/thresholding.py
@@ -1,9 +1,17 @@
 import numpy as np
 
+def find_average(arr):
+    return arr[0]*0.299+arr[1]*0.587+arr[2]*0.114
+
 def effect_thresholding(img_matrix):
     T = 127
     output_img_matrix = np.copy(img_matrix)
-    for index, value in np.ndenumerate(output_img_matrix):
-        output_img_matrix[index] = 255 if value > T else 0
+    for x in range(output_img_matrix.shape[0]):
+        for y in range(output_img_matrix.shape[1]):
+            average_value = find_average(output_img_matrix[x, y])
+            if average_value > T:
+                output_img_matrix[x,y] = [255, 255, 255]
+            else:
+                output_img_matrix[x, y] = [0, 0, 0]
 
     return output_img_matrix
